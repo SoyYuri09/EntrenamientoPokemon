db8681029af4e9a59f9aaf89868c5d2be8ac2099
Daren <yuri.garcia252583@potros.itson.edu.mx>
2026-03-13 01:36:01 -0700

    Creación del modelo Pokemon

--- EntrenamientoPokemon/app/src/main/java/yuri/garcia/entrenamientopokemon_252583_252801_253080/data/models/Pokemon.kt
+++ EntrenamientoPokemon/app/src/main/java/yuri/garcia/entrenamientopokemon_252583_252801_253080/data/models/Pokemon.kt
@@ -1,4 +1,10 @@
 package yuri.garcia.entrenamientopokemon_252583_252801_253080.data.models
 
-class Pokemon {
-}
+data class Pokemon(
+    val name: String,
+    val numPkdx: Int,
+    val type: String,
+    val height: Double,
+    val weight: Double,
+    val description: String
+)
